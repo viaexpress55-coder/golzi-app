b142f33f8dbc87f6154a643f3ff867bb4471fd0e
Fernando Galicia <viaexpress55@gmail.com>
2026-06-02 18:22:27 -0500

    chore: remove temp fix scripts

--- fix_check.js
+++
@@ -1,4 +0,0 @@
-const fs = require('fs');
-const c = fs.readFileSync('src/screens/register/RegisterScreen.tsx', 'utf8');
-const i = c.indexOf('setLoading(true)');
-console.log(JSON.stringify(c.substring(i-30, i+80)));
